ae4c9206a709d6a5886be53a4406aac55781aba8
Naveen Pitipornvivat <naveen.p@KB-XXAXXD9PWW.kasikornbank.com>
2024-11-20 16:24:09 +0700

    commit tutorial

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,14 +1,21 @@
-<script>
+<script setup>
 // TODO: 4. ทำความรู้จัก useHead()
 // TODO: 5. ลอง console.log เพื่อดู log
+console.log("hello world")
+useHead({
+    title:'Hello World'
+})
 </script>
 
-
 <template>
+    <span>Hello World</span>
     <!-- TODO: 1. ทำความรู้จัก template tag -->
     <!-- TODO: 2. แก้ tailwind.config.js เพื่อให้ตอน build  -->
 </template>
 
 <style lang="css">
 /** TODO: 3. ใส่ CSS ให้กับ HTML */
+body {
+    background-color: darkgray;
+}
 </style>
