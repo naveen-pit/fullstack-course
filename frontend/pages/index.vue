96d3e190ff1d617b91698cce2517ffc9b514f85e
Naveen Pitipornvivat <naveen.p@KB-XXAXXD9PWW.kasikornbank.com>
2024-12-04 16:37:01 +0700

    add component

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,6 @@
 <template>
     หน้าแรก
+    <BaseLink to="/">Go to Home</BaseLink>
+    <BaseButton size="small">Hello World</BaseButton>
     <!-- TODO: 5. นำมาใช้งาน และรู้จัก Fallthrough -->
 </template>
